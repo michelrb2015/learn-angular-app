83100eab0f52e7e9a0fe4ee8a8bba2f858221cd3
Michel Reynaldo Brito <michel.brito@growthaccelerationpartners.com>
2021-12-03 23:53:33 -0500

    Template Driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { NgForm, FormGroup } from '@angular/forms';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learn-angular-app';
+export class AppComponent implements OnInit {
+  title = 'Template driven forms';
+ 
+  @ViewChild('contactForm') contactForm!: NgForm;
+ 
+  countryList: country[] = [
+    new country("1", "India"),
+    new country('2', 'USA'),
+    new country('3', 'England')
+  ];
+ 
+  contact!: contact;
+ 
+  ngOnInit() {
+ 
+    this.contact = {
+      firstname: "Sachem",
+      lastname: "Tendulkar",
+      email: "sachin@gmail.com",
+      gender: "male",
+      address: {
+        country: "2",
+        city: "Mumbai",
+        street: "Perry Cross Rd",
+        pincode: "400050"
+      }
+    };
+ 
+    setTimeout(() => {
+      this.contactForm.setValue(this.contact);
+    });
+ 
+  }
+ 
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+ 
+  setDefaults() {
+    this.contactForm.setValue(this.contact);
+  }
+ 
+  changeCountry() {
+    let obj = {
+      country: "1",
+      city: "",
+      street: "",
+      pincode: ""
+    };
+    this.contactForm.controls["address"].setValue(obj);
+  }
+ 
+  patchValue() {
+    let obj = {
+      firstname: "Raul",
+      lastname: "David",
+      email: "rahul@gmail.com",
+    };
+ 
+    this.contactForm.control.patchValue(obj);
+ 
+  }
+ 
+  changeAddress() {
+    let obj = {
+      country: "3",
+      city: "Bangalore",
+      street: "Brigade Road",
+      pincode: "600100"
+    };
+    let address= this.contactForm.controls["address"] as FormGroup
+    address.patchValue(obj);
+ 
+  }
+ 
+  reset() {
+    this.contactForm.reset();
+  }
+ 
+  resetForm() {
+    this.contactForm.resetForm();
+  }
+}
+ 
+ export class contact {
+  firstname!: string;
+  lastname!: string;
+  email!: string;
+  gender!: string;
+  address!: {
+    country: string;
+    city: string;
+    street: string;
+    pincode: string;
+  };
+}
+ 
+ 
+export class country {
+  id: string;
+  name: string;
+ 
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+/**
+ * Material's imports
+ */
+ import {MatFormFieldModule} from '@angular/material/form-field';
+ import {MatInputModule} from '@angular/material/input';
+ import {MatCardModule} from '@angular/material/card';
+ import {MatRadioModule} from '@angular/material/radio';
+ import {MatSelectModule} from '@angular/material/select';
+ import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +22,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+
+    //? Material modules
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatButtonModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
